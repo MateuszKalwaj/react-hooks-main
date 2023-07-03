e04a260425f2e0817101d6572a61634f155ac242
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2023-07-03 14:12:30 +0200

    01 useState: add initialName to the props and state

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,9 +4,9 @@
 import * as React from 'react'
 import {useState} from "react";
 
-//Solution
-function Greeting() {
-  const [name, setName] = useState('');
+
+function Greeting({initialName = ''}) {
+  const [name, setName] = useState(initialName);
 
   function handleChange(event) {
     setName(event.target.value);
@@ -16,7 +16,7 @@ function Greeting() {
     <div>
       <form>
         <label htmlFor="name">Name: </label>
-        <input onChange={handleChange} id="name" />
+        <input value={name} onChange={handleChange} id="name" />
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -24,7 +24,8 @@ function Greeting() {
 }
 
 function App() {
-  return <Greeting />
+  // return <Greeting />
+  return <Greeting initialName={"Mike Hunt"}/>
 }
 
-export default App
+export default App;
