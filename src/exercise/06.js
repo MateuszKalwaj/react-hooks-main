869164a5e155cf580c56dd0fc3e198592a7630e0
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2023-07-20 22:21:52 +0200

    06 :httpRequests handle error

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -10,40 +10,50 @@ import {useEffect, useState} from "react";
 
 function PokemonInfo({pokemonName}) {
     const [pokemon, setPokemon] = useState(null)
-    useEffect(()=>{
+    const [error, setError] = useState('');
+
+    useEffect(() => {
         if (!pokemonName) {
             return
         }
         setPokemon(null)
-        fetchPokemon(pokemonName).then(pokemonData=> setPokemon(pokemonData))
-    },[pokemonName])
+
+        fetchPokemon(pokemonName).then(
+            pokemonData => {setPokemon(pokemonData)},
+        ).catch(error => setError(error))
+    }, [pokemonName])
+
+    if (error) {
+        return <div role="alert">
+            There was an error: <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+        </div>
+    }
 
     if (!pokemonName) {
-        console.log("Pokemon should be submitted")
         return 'submit pokemon'
     } else if (!pokemon) {
-      return  <PokemonInfoFallback name={pokemonName} />
+        return <PokemonInfoFallback name={pokemonName}/>
     } else {
-        return <PokemonDataView pokemon={pokemon} />
+        return <PokemonDataView pokemon={pokemon}/>
     }
 }
 
 function App() {
-  const [pokemonName, setPokemonName] = React.useState('')
+    const [pokemonName, setPokemonName] = React.useState('')
 
-  function handleSubmit(newPokemonName) {
-    setPokemonName(newPokemonName)
-  }
+    function handleSubmit(newPokemonName) {
+        setPokemonName(newPokemonName)
+    }
 
-  return (
-    <div className="pokemon-info-app">
-      <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit} />
-      <hr />
-      <div className="pokemon-info">
-        <PokemonInfo pokemonName={pokemonName} />
-      </div>
-    </div>
-  )
+    return (
+        <div className="pokemon-info-app">
+            <PokemonForm pokemonName={pokemonName} onSubmit={handleSubmit}/>
+            <hr/>
+            <div className="pokemon-info">
+                <PokemonInfo pokemonName={pokemonName}/>
+            </div>
+        </div>
+    )
 }
 
 export default App
