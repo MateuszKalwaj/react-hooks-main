af1ec702f2fce3085fd1a7d8950a5a4f5ec00d22
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2024-09-11 17:35:00 +0200

    updated course_basic hooks

--- src/final/01.js
+++ src/final/01.js
@@ -2,12 +2,16 @@
 // http://localhost:3000/isolated/final/01.js
 
 import * as React from 'react'
+import {useState} from "react";
 
 function Greeting() {
-  const [name, setName] = React.useState('')
+  const [name, setName]= useState('');
+
   function handleChange(event) {
-    setName(event.target.value)
+    setName(event.target.value);
   }
+  console.log(name)
+
   return (
     <div>
       <form>
