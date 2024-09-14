5753ea12426a55f3bc1f47ebd9207ba00addee26
mateuszkalwaj <mateusz.kalwaj@gmail.com>
2024-09-14 10:34:03 +0200

    useCounter() custom hook added

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -2,47 +2,43 @@
 // http://localhost:3000/isolated/exercise/02.js
 
 import * as React from 'react'
-import {useState} from "react";
-
-function useLocalStorageState(key, defaultValue) {
-  const [state, setState] = React.useState(
-      ()=> window.localStorage.getItem(key) || defaultValue
-  )
-
-  React.useEffect(()=> {
-    window.localStorage.setItem(key, state);
-  },[state])
-
-return [state, setState]
-}
+import {useEffect, useState} from "react";
 
 function Greeting({initialName = ''}) {
-  const [name, setName] = useLocalStorageState('name',initialName)
+    console.log("calling greetings")
+    const [name, setName] = useState(()=> localStorage.getItem('name') || initialName);
 
-  function handleChange(event) {
-    setName(event.target.value)
-  }
-  return (
-    <div>
-      <form>
-        <label htmlFor="name">Name: </label>
-        <input value={name} onChange={handleChange} id="name" />
-      </form>
-      {name ? <strong>Hello {name}</strong> : 'Please type your name'}
-    </div>
-  )
+    useEffect(() => {
+        console.log("calling useEffect")
+         localStorage.setItem('name', name);
+    })
+
+    function handleChange(event) {
+        setName(event.target.value)
+    }
+    return (
+        <div>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input value={name} onChange={handleChange} id="name" />
+            </form>
+            {name ? <strong>Hello {name}</strong> : 'Please type your name'}
+        </div>
+    )
+}
+function useCounter() {
+    const [count, setCount] = useState(0);
+    const increment = () => setCount(prevState => prevState + 1);
+    return [count, increment];
 }
 
+
 function App() {
-  const [count, setCount] = useState(0);
-  return(
-      <>
-        <button onClick={()=> setCount(prevCount => prevCount + 1)}>
-          {count}
-        </button>
-        <Greeting initialName={'Mike Hunt'} />
-      </>
-  )
+    const [count, setCount] = useCounter();
+    return <>
+        <Greeting initialName={"sir Mateusz"} />
+        <button onClick={setCount}>{count}</button>
+    </>
 }
 
 export default App
